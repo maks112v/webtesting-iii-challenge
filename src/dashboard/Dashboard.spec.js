6f512673511521adc9bf0889f37fdc2e5aad8d45
Maksim <maks112v@gmail.com>
2019-04-10 23:52:14 -0400

    updates

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 // Test away
 import React from "react";
-import { render, fireEvent, cleanup } from "react-testing-library";
+import { render, cleanup } from "react-testing-library";
 import renderer from 'react-test-renderer'
 
 import Dashboard from "./Dashboard";
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from 'react';
-import { render, fireEvent, cleanup } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
 
 import Display from './Display'
 
